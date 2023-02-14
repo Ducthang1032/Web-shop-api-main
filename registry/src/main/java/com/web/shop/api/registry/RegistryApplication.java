1cf513481c4616dbffd705cef6b9905c6c364d50
thangnguyen2 <thangnguyen2@tdt.asia>
2023-02-14 10:06:16 +0700

    Add module registry Eureka server

--- registry/src/main/java/com/web/shop/api/registry/RegistryApplication.java
+++ registry/src/main/java/com/web/shop/api/registry/RegistryApplication.java
@@ -1,4 +1,4 @@
-package com.example.registry;
+package com.web.shop.api.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- registry/src/test/java/com/web/shop/api/registry/RegistryApplicationTests.java
+++ registry/src/test/java/com/web/shop/api/registry/RegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.registry;
+package com.web.shop.api.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
