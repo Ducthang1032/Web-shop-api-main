839ac07a51337d53b676718c68fefbca4423eaea
thangnguyen2 <thangnguyen2@tdt.asia>
2023-02-14 09:52:47 +0700

    Add maven

---
+++ src/test/java/com/web/shop/api/Web/Shop/API/WebShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.shop.api.Web.Shop.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
